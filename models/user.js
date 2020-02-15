a1bbdff14d7bfa41a8374343a9b329e6db265209
Miguel <alonsogalalrdomiguel@gmail.com>
2020-02-15 21:28:45 +0100

    add formulary login and register, add formatOnSave, add local Strategy, add css and script bootstrap, add local passport strategy

--- models/user.js
+++ models/user.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String,
-  address: String,
-}, {
-  timestamps: true
-});
+const userSchema = new Schema(
+  {
+    name: String,
+    lastName: String,
+    username: String,
+    password: String,
+    address: String,
+    city: String,
+    zip: Number
+  },
+  {
+    timestamps: true
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
