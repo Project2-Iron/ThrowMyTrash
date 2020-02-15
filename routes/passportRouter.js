af2b6c361b9e10340e7ed89c79ab6e0083d70e4c
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-02-15 13:03:03 +0100

    prueba

---
+++ routes/passportRouter.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const passportRouter = express.Router();
+const ensureLogin = require("connect-ensure-login");
+const model = require("../models/user");
+const passport = require("passport");
+const { hashPassword, checkHashed } = require("../lib/hashing");
+
+passportRouter.get("/register", (req, res, next) => {
+  res.render("passport/register");
+});
+
+passportRouter.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  console.log("username");
+  const userCreated = await model.findOne({ username });
+
+  if (userCreated) {
+    return res.redirect("/register");
+  } else {
+    await model.create({
+      username,
+      password: hashPassword(password)
+    });
+  }
+
+  return res.redirect("/");
+});
+
+passportRouter.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+passportRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+passportRouter.get("/logout", ensureLogin.ensureLoggedIn(), (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+// passportRouter.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
+//   res.render("passport/private", { user: req.user });
+// });
+
+module.exports = passportRouter;
