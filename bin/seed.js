429576a0f06d704cfe4abb20a347a81df64ca447
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-02-19 20:17:30 +0100

    seedmap

--- bin/seed.js
+++ bin/seed.js
@@ -1,2 +1,13 @@
 const data = require("../data/convertcsv.json");
-console.log(data);
+// console.log(data);
+
+const containers = data.map(e => {
+  const container = {};
+  container.type = e["Tipo Contenedor"];
+  container.lng = e.LONGITUD;
+  container.lat = e.LATITUD;
+  container.street = e.NOMBRE;
+  return container;
+});
+
+console.log(containers);
