87cc3cc770fe59abc50646bf4166c6c670bdddb6
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-02-29 12:17:20 +0100

    start

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -11,7 +11,7 @@ window.cpointsApi = axios.create({
 
 window.setMap = (center, zoom = 10) => {
   const map = new mapboxgl.Map({
-    container: "maptrash",
+    container: "map",
     style: "mapbox://styles/mapbox/streets-v11",
     center,
     zoom
@@ -25,26 +25,5 @@ window.setMap = (center, zoom = 10) => {
       trackUserLocation: true
     })
   );
-
-  return map;
-};
-
-window.setMap = (center, zoom = 10) => {
-  const map = new mapboxgl.Map({
-    container: "mapcpoints",
-    style: "mapbox://styles/mapbox/streets-v11",
-    center,
-    zoom
-  });
-  map.addControl(new mapboxgl.NavigationControl());
-  map.addControl(
-    new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true
-    })
-  );
-
   return map;
 };
